35077afd87562ed20280b436c696f1c729f80012
haiderarbaz <admin@arbazhaider.com>
2025-05-07 12:18:03 +0000

    add hello-world folder and test.cpp file to repo

---
+++ hello-world/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "✅ Hello from Arbaz Haider! Your setup works! 🎉" << endl;
+}
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "✅ Hello from C++! Your setup works! 🎉" << endl;
+    return 0;
+}
