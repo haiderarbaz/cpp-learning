05e0fa131ddf6c6864926a635a29446f1a4ba5e9
Arbaz Haider <admin@arbazhaider.com>
2025-05-08 23:37:13 +0530

    updated folder name serial number wise

---
+++ 00-basics/basic04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// without using "namespace" we can also write like this
+
+using std::cout;
+using std::endl;
+
+int main(){
+    cout << "Hi ! Arbaz" << endl;
+    cout << "Hello from C++ ! Your setup works fine." << endl;
+    cout << "Happy Coding" << endl;
+    return 0;
+}
