d29101e68cf9b55a237016173ab1f8accd873c71
Arbaz Haider <admin@arbazhaider.com>
2025-06-03 02:13:04 +0530

    add basicode file to codebase

---
+++ 00-basics/basic08.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello\tWorld\n";
+    cout << "She, said, \"Hi!\"\n";
+    cout << "Path: C:\\program files";
+
+
+    return 0;
+}
