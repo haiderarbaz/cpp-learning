f4f223239d8e12cea0de96a18f005f2a78684312
haiderarbaz <admin@arbazhaider.com>
2025-09-14 15:04:11 +0000

    add ternary code file to 03-operator/ dir

---
+++ 03-operators/ternary.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+
+    int a, b, c;
+    a = 3;
+    b = 9;
+    c = (a > b) ? a : b;
+    cout << "c: " << c << endl;
+
+
+    return 0;
+}
